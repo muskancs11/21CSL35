eb33e67dae6d401edc0b2f9fd5bb5ef4f508b969
Kritika Taank <taank.kritika@gmail.com>
2022-12-22 09:43:14 +0530

    Update prime-number.java

--- prime-number.java
+++ prime-number.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class eeehhe {
 
-	public static void main(String[] args){		
+	public static void main(String[] args){	
+		
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter value to check: ");
